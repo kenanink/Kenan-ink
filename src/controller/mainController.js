bbf4935ec45cfb58d7ce5531e3caf558c3d05939
Kevin Barrera <Kevinbarrera1@icloud.com>
2026-01-06 00:58:44 -0500

    new

--- src/controller/mainController.js
+++
@@ -1,9 +0,0 @@
-const controller = {
-    index: (req,res)=>{
-        res.render('index');
-    },
-    about: (req,res)=>{
-        res.render('./about')
-    },
-}
-module.exports = controller;
--- src/routes/mainRoute.js
+++
@@ -1,9 +0,0 @@
-const express=require('express');
-const router=express.Router();
-
-const mainController = require('../controller/mainController');
-
-router.get('/', mainController.index);
-router.get('/about', mainController.about);
-
-module.exports = router;
